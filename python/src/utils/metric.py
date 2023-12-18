25ff78a553a7c84e94e70a29b60059c7f7fe3ea2
KugaMaxx <KugaMaxx@outlook.com>
2023-12-18 11:23:47 +0800

    feat(#5): add `EventStructuralRatio()` used in the original article
    ---
    Add another ESR version that can reproduce the original experimental results.
    
      - python/src/utils/metric.py
        * line  109 to  191: old version of esr
    
    ---
    Resolves: #5

--- python/src/utils/metric.py
+++ python/src/utils/metric.py
@@ -21,7 +21,7 @@ def median_filter(data, size=3):
     return filtered
 
 
-class EventStructuralRatio(object):
+class EventStructuralRatioV2(object):
     def __init__(self, resolution) -> None:
         self.resolution = resolution
         self.accumulator = dv.Accumulator(resolution)
@@ -94,7 +94,7 @@ class EventStructuralRatio(object):
         # get basic info
         n = median_filter(self.accumulator.getPotentialSurface(), size=3)
         N = events.size()
-        K = self.resolution[0] * self.resolution[1]
+        K = self.resolution[0] * self.resolution[1]  # n.size
 
         # calculate ntss
         ntss = (n * n).sum() / (N * N)
@@ -104,3 +104,88 @@ class EventStructuralRatio(object):
 
         # return esr
         return 1000 * np.sqrt(ntss * ln)
+
+
+class EventStructuralRatio(object):
+    def __init__(self, resolution) -> None:
+        self.resolution = resolution
+        self.accumulator = dv.Accumulator(resolution)
+
+        # set accumulator
+        self.accumulator.setMinPotential(-np.inf)
+        self.accumulator.setMaxPotential(np.inf)
+        self.accumulator.setEventContribution(1.0)
+        self.accumulator.setIgnorePolarity(True)
+        self.accumulator.setDecayFunction(dv.Accumulator.Decay.NONE)
+    
+    def evalPerTimeInterval(self, data, 
+                            reference: str = "events",
+                            interval: timedelta = timedelta(milliseconds=33)):
+        # slice data
+        slicer, score = kit.MonoCameraSlicer(), list()
+        slicer.doEveryTimeInterval(
+            reference, interval, 
+            lambda data: score.append(self._calc_esr(data["events"].toEventStore()))
+        )
+        slicer.accept(data)
+
+        # return result
+        return np.array(score)
+
+    def evalPerNumberInterval(self, data, 
+                              reference: str = "events",
+                              interval: int = 30000):
+        # slice data
+        slicer, score = kit.MonoCameraSlicer(), list()
+        slicer.doEveryNumberOfElements(
+            reference, interval, 
+            lambda data: score.append(self._calc_esr(data["events"].toEventStore()))
+        )
+        slicer.accept(data)
+        
+        # return result
+        return np.array(score)
+    
+    def evalEventStorePerTime(self, events: dv.EventStore, 
+                              interval: timedelta = timedelta(milliseconds=33)):
+        # slice data
+        slicer, score = dv.EventStreamSlicer(), list()
+        slicer.doEveryTimeInterval(
+            interval, 
+            lambda events: score.append(self._calc_esr(events))
+        )
+        slicer.accept(events)
+        
+        # return result
+        return np.array(score)
+
+    def evalEventStorePerNumber(self, events: dv.EventStore, 
+                                interval: int = 30000):
+        # slice data
+        slicer, score = dv.EventStreamSlicer(), list()
+        slicer.doEveryNumberOfEvents(
+            interval, 
+            lambda events: score.append(self._calc_esr(events))
+        )
+        slicer.accept(events)
+        
+        # return result
+        return np.array(score)
+
+    def _calc_esr(self, events):
+        self.accumulator.clear()
+        self.accumulator.accept(events)
+        
+        # get basic info
+        n = median_filter(self.accumulator.getPotentialSurface(), size=3)
+        N, M = events.size(), int(events.size() * 2 / 3)
+        K = self.resolution[0] * self.resolution[1]  # n.size
+
+        # calculate ntss
+        ntss = (n * (n - 1)).sum() / (N + np.spacing(1)) / (N - 1 + np.spacing(1))
+        
+        # calculate ln
+        ln = K - ((1 - M / N) ** n).sum()
+
+        # return esr
+        return np.sqrt(ntss * ln)
